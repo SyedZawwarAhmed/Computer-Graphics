e8cf2f7b56d0ca3649298950b9ffd962e3587b0e
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-07-22 15:37:41 +0500

    comments added

--- Main.cpp
+++ Main.cpp
@@ -2,27 +2,33 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+// Vertex shader source code
 const char* vertexShaderSource = "#version 330 core\n"
-"layour (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
+"layout (location = 0) in vec3 aPos;\n"  // Define the input attribute 'aPos' for vertex positions
 "void main()\n"
 "{\n"
-"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"  // Set the position of the vertex
+"}\0";
+
+// Fragment shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"  // Define the output color of the fragment shader
+"void main()\n"
+"{\n"
+"    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"  // Set the output color for fragments
 "}\n\0";
 
-int main() 
+int main()
 {
+	// Initialize GLFW
 	glfwInit();
 
+	// Set OpenGL version and core profile
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Define vertices of a triangle in 3D space
 	GLfloat vertices[] =
 	{
 		-0.5f, -0.5 * float(sqrt(3)) / 3, 0.0f,
@@ -30,6 +36,7 @@ int main()
 		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
 	};
 
+	// Create a GLFW window
 	GLFWwindow* window = glfwCreateWindow(800, 800, "MyFirstOpenGLProject", NULL, NULL);
 	if (window == NULL)
 	{
@@ -39,66 +46,87 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	// Load OpenGL function pointers with GLAD
 	gladLoadGL();
 
+	// Set the viewport
 	glViewport(0, 0, 800, 800);
 
+	// Create and compile the vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
+	// Create and compile the fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
+	// Create the shader program and link the shaders
 	GLuint shaderProgram = glCreateProgram();
-
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
+	// Cleanup the shader objects
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// Create Vertex Array Object (VAO) and Vertex Buffer Object (VBO)
 	GLuint VAO, VBO;
-
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
+	// Bind VAO and VBO
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+	// Copy vertex data into the VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
+	// Set the vertex attribute pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glad_glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(0);
 
+	// Unbind VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
+	// Set the clear color (background color)
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
+	// Clear the color buffer and swap the front and back buffers
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
+	// Main render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use the shader program
 		glUseProgram(shaderProgram);
+
+		// Bind the VAO
 		glBindVertexArray(VAO);
+
+		// Draw the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+		// Swap front and back buffers
 		glfwSwapBuffers(window);
 
+		// Poll for events
 		glfwPollEvents();
 	}
 
+	// Cleanup and delete resources
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteProgram(shaderProgram);
 
+	// Destroy the window and terminate GLFW
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
